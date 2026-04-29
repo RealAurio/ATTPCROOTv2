0f3b5d6d249fb7e831641091dde25bc007e14b58
Tim Gray <tgray30@utk.edu>
2026-04-29 12:33:52 -0400

    fixed minor bugs in the Si PSA to correctly process negative traces. Several GAGG channels are saturating the ADC, so (a) detecting and accounting for when overflows happen, and (b) fitting the non-clipped part of the trace to have some estimate of the total energy

--- AtReconstruction/AtPulseAnalyzer/AtPSASi.cxx
+++ AtReconstruction/AtPulseAnalyzer/AtPSASi.cxx
@@ -3,6 +3,9 @@
 #include "AtHit.h"
 #include "AtPad.h"
 
+#include <TGraph.h>
+#include <TF1.h>
+#include <TMath.h>
 #include <FairLogger.h>
 
 #include <Math/Point3D.h>    // for PositionVector3D
@@ -53,16 +56,27 @@ AtPSASi::HitVector AtPSASi::AnalyzePad(AtPad *pad)
    // Get baseline value.
    double baseline{};
    Double_t *maxAdcIt{nullptr};
+   Double_t maxAdc{};
    Double_t charge{-999};
    Int_t maxAdcIdx;
    if (fPositivePolarity) {
       maxAdcIt = std::max_element(floatADC.begin() + 20, floatADC.end() - 12);
       maxAdcIdx = std::distance(floatADC.begin(), maxAdcIt);
+      maxAdc = *maxAdcIt; //copy value to avoid changing floatADC
    } else {
       maxAdcIt = std::min_element(floatADC.begin() + 20, floatADC.end() - 12);
       maxAdcIdx = std::distance(floatADC.begin(), maxAdcIt);
-      (*maxAdcIt) = -(*maxAdcIt);
+      maxAdc = *maxAdcIt; //copy value to avoid changing floatADC
    }
+
+   //std::ofstream ofile("traces.dat", std::ios::app);
+   //if (abs(*maxAdcIt - baseline) > 50) {
+   //  for (int i=0; i<512; ++i) {
+   //    ofile << floatADC[i] << "  ";
+   //  }
+   //  ofile << std::endl;
+   //}
+   //ofile.close();
 
    float count = 0;
    for (int i = std::max(20, maxAdcIdx + low_bl_region[0]); i < std::min(maxAdcIdx + low_bl_region[1], 500);
@@ -75,12 +89,18 @@ AtPSASi::HitVector AtPSASi::AnalyzePad(AtPad *pad)
       baseline += floatADC[i];
       count += 1;
    }
+
    baseline /= count;
    LOG(debug) << "Baseline calculated: " << baseline;
-   charge = *maxAdcIt - baseline;
+   charge = maxAdc - baseline;
+
+   if (!fPositivePolarity) {
+     charge *= -1;
+   }
 
    if (!shouldSaveHit(charge, fThreshold, maxAdcIdx))
       return {};
+
 
    LOG(debug) << "========== float ADC array max value ========== " << *maxAdcIt;
    LOG(debug) << "========== float ADC array max index: " << maxAdcIdx << " ==========";
@@ -150,8 +170,13 @@ AtPSASi::HitVector AtPSASi::AnalyzeGenTrace(AtGenericTrace *genTrace)
    if (floatADCVector.size() >= 256) {
       for (int i = 0; i < 256; i++) {
          floatADC[i] = floatADCVector[i];
+         //if (i>0) {
+         //  if (floatADC[i-1] > 3500 && floatADC[i] < (floatADC[i-1]-4096+500)) {
+         //    floatADC[i] += 4096; //overflow
+         //  }
+         //}
          if (i>0) {
-           if (floatADC[i-1] > 3500 && floatADC[i] < (floatADC[i-1]-4096+500)) {
+           if ((floatADC[i-1] - floatADC[i]) > 1000) {
              floatADC[i] += 4096; //overflow
            }
          }
@@ -161,19 +186,43 @@ AtPSASi::HitVector AtPSASi::AnalyzeGenTrace(AtGenericTrace *genTrace)
       return {};
    }
 
-   Double_t *maxAdcIt{nullptr};
+   //Double_t *maxAdcIt{nullptr};
    Double_t charge{-999};
    Double_t baseline{0};
-   if (fPositivePolarity) {
-      maxAdcIt = std::max_element(floatADC.begin() + 20, floatADC.end() - 12);
-      charge = *maxAdcIt;
-   } else {
-      maxAdcIt = std::min_element(floatADC.begin() + 20, floatADC.end() - 12);
-      charge = -(*maxAdcIt);
+   Int_t maxAdcIdx=-1;
+   Double_t maxAdc=-1;
+   int min_overflow=-1;
+   int max_overflow=-1;
+   
+   //if (fPositivePolarity) {
+   //   maxAdcIt = std::max_element(floatADC.begin() + 20, floatADC.end() - 12);
+   //   charge = *maxAdcIt;
+   //} else {
+   //   maxAdcIt = std::min_element(floatADC.begin() + 20, floatADC.end() - 12);
+   //   charge = -(*maxAdcIt);
+   //}
+   for (int i=20; i<floatADCVector.size()-12; ++i) {
+     if (floatADC[i] > maxAdc) { 
+       maxAdc = floatADC[i];
+       maxAdcIdx = i;
+     }
+     if (floatADC[i] == 8191) { //overflow hack
+       if (min_overflow == -1) {
+         min_overflow = i;
+       }
+       else {
+         max_overflow = i;
+       }
+     }
    }
-   Int_t maxAdcIdx = std::distance(
-      floatADC.begin(),
-      maxAdcIt); // To-Do: might need to utilize similar trace intergal calculation as used in Si. Unsure yet.
+   bool saturation = false;
+   if (min_overflow > -1 && max_overflow > -1) {
+     saturation = true;
+     maxAdcIdx = (int)((min_overflow+max_overflow)/2);
+   }
+   //Int_t maxAdcIdx = std::distance(
+   //   floatADC.begin(),
+   //   maxAdcIt); // To-Do: might need to utilize similar trace intergal calculation as used in Si. Unsure yet.
 
    float count = 0;
    for (int i = std::max(20, maxAdcIdx + low_bl_region[0]); i < std::min(maxAdcIdx + low_bl_region[1], (int)(floatADCVector.size()-12));
@@ -188,24 +237,25 @@ AtPSASi::HitVector AtPSASi::AnalyzeGenTrace(AtGenericTrace *genTrace)
    }
    baseline /= count;
    LOG(debug) << "Baseline calculated: " << baseline;
-   charge = *maxAdcIt - baseline;
+   charge = maxAdc - baseline;
 
-   /*
-   if (std::atoi(genTrace->GetName().c_str())==1906 && genTrace->GetTraceID()==6) {
-     std::cout << " Max ADC = " << *maxAdcIt << std::endl;
-     std::cout << "Baseline calculated: " << baseline;
-     std::cout << " Diff ADC = " << *maxAdcIt - baseline << std::endl;
+   
+   //if (std::atoi(genTrace->GetName().c_str())==1906 && genTrace->GetTraceID()==6) {
+   //if (std::atoi(genTrace->GetName().c_str())==1905 && genTrace->GetTraceID()==0) {
+   //  std::cout << " Max ADC = " << *maxAdcIt << std::endl;
+   //  std::cout << "Baseline calculated: " << baseline;
+   //  std::cout << " Diff ADC = " << *maxAdcIt - baseline << std::endl;
 
-     std::ofstream ofile("traces.dat", std::ios::app);
-     if (abs(*maxAdcIt - baseline) > 50) {
-       for (int i=0; i<256; ++i) {
-         ofile << floatADC[i] << "  ";
-       }
-       ofile << std::endl;
-     }
-     ofile.close();
-   }
-   */
+   //  std::ofstream ofile("traces.dat", std::ios::app);
+   //  if (abs(*maxAdcIt - baseline) > 50) {
+   //    for (int i=0; i<256; ++i) {
+   //      ofile << floatADC[i] << "  ";
+   //    }
+   //    ofile << std::endl;
+   //  }
+   //  ofile.close();
+   //}
+   
 
    if (!shouldSaveHit(charge, fThreshold, maxAdcIdx)) {
       LOG(debug) << "GAGG trace did not pass threshold.";
@@ -219,6 +269,45 @@ AtPSASi::HitVector AtPSASi::AnalyzeGenTrace(AtGenericTrace *genTrace)
                                                floatADC.begin() + maxAdcIdx + energy_integral[1], 0) /
                                   (energy_integral[1] - energy_integral[0]) -
                                baseline);
+   if (saturation) { //fit the good part of the trace to extrapolate the peak
+     //using a TGraph and a TF1 now because it's an easy interaface. May well be a more efficent way to go about this 
+     TGraph gr;
+     int n = 0;
+     for (int i = std::max(20, maxAdcIdx - 40); i < std::min(maxAdcIdx + 50, (int)(floatADCVector.size()-12));
+         i++) { // more baseline added
+       if (floatADC[i] < 8191) { //overflow hack
+          gr.SetPoint(n, i, floatADC[i] );
+          ++n;
+       }
+     }
+     //params are
+     //[0] : baseline
+     //[1] : amplitude
+     //[2] : mean
+     //[3] : sigma
+     //[4] : skewness
+     //
+     std::cout << "saturated GAGG with "<< n << " points to fit" << std::endl;
+     
+     TF1 func("func", [&](double *x, double *p) { return p[0]+p[1]*p[4]*exp(p[4]/2. * (2.*p[2] + p[4]*p[3]*p[3] - 2.*x[0])) * TMath::Erfc((p[2] + p[4]*p[3]*p[3] - x[0])/(sqrt(2)*p[3])); },0,256,5);
+     func.FixParameter(0, baseline);
+     func.SetParameter(1, 10000);
+     func.SetParLimits(1, 0, 500000);
+     func.SetParameter(2, maxAdcIdx);
+     func.SetParLimits(2, min_overflow, max_overflow);
+     func.FixParameter(3, 3.7);
+     func.FixParameter(4, 0.2);
+     gr.Fit(&func, "R");
+     func.ReleaseParameter(0);
+     func.SetParLimits(0, baseline*0.5, baseline*1.5);
+     func.ReleaseParameter(3);
+     func.SetParLimits(3, 3.0, 6.0);
+     func.ReleaseParameter(4);
+     func.SetParLimits(4, 0.05, 1.5);
+     gr.Fit(&func, "R");
+     QHitTot = func.GetMaximum() - baseline; //this is not quite correct since QHitTot represents an integral not the maximum
+     std::cout << "saturated GAGG fit to " << func.GetParameter(1) << "  " << func.GetParameter(3) << "  " << func.GetParameter(4) << "  " <<  QHitTot << std::endl;
+   }
 
    auto hit = std::make_unique<AtHit>(0, pos, charge);
 
